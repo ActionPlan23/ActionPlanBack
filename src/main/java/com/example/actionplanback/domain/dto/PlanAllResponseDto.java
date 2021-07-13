0fc2be3607569bcdae1a7a668b29521611c60435
icwgi <dhkdrb897@naver.com>
2021-07-13 10:11:59 +0900

    Refactoring 연관관계 양방향 -> 단방향으로 수정

--- src/main/java/com/example/actionplanback/domain/dto/PlanAllResponseDto.java
+++ src/main/java/com/example/actionplanback/domain/dto/PlanAllResponseDto.java
@@ -2,11 +2,13 @@ package com.example.actionplanback.domain.dto;
 
 
 import com.example.actionplanback.domain.entity.Plan;
+import com.example.actionplanback.domain.entity.Reply;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 
 @Getter
@@ -16,20 +18,17 @@ public class PlanAllResponseDto {
     private String title;
     private String content;
     private String writer;
-    private String planPassword;
     private int countReply;
     private LocalDateTime createdAt;
     private LocalDateTime modifiedAt;
 
-    public PlanAllResponseDto(Plan plan){
+    public PlanAllResponseDto(Plan plan, List<Reply> replyList){
 
         this.planId = plan.getPlanId();
         this.title = plan.getTitle();
         this.content = plan.getContent();
         this.writer = plan.getPlanWriter();
-        this.planPassword = plan.getPlanPassword();
-        this.countReply = plan.getReplyList().size();
-
+        this.countReply = replyList.size();
         this.createdAt = plan.getCreatedAt();
         this.modifiedAt = plan.getModifiedAt();
     }
--- src/main/java/com/example/actionplanback/service/PlanService.java
+++ src/main/java/com/example/actionplanback/service/PlanService.java
@@ -1,11 +1,10 @@
 package com.example.actionplanback.service;
 
-import com.example.actionplanback.domain.dto.DeleteRequestDto;
-import com.example.actionplanback.domain.dto.PlanAllResponseDto;
-import com.example.actionplanback.domain.dto.PlanDetailResponseDto;
-import com.example.actionplanback.domain.dto.PlanRequestDto;
+import com.example.actionplanback.domain.dto.*;
 import com.example.actionplanback.domain.entity.Plan;
+import com.example.actionplanback.domain.entity.Reply;
 import com.example.actionplanback.domain.repository.PlanRepository;
+import com.example.actionplanback.domain.repository.ReplyRepository;
 import com.example.actionplanback.exception.ApiRequestException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -20,16 +19,18 @@ import java.util.stream.Collectors;
 public class PlanService {
 
     private final PlanRepository planRepository;
+    private final ReplyRepository replyRepository;
 
     // 리스트 조회
-    @Transactional
+    @Transactional //List<PlanAllResponseDto>원본
     public List<PlanAllResponseDto> getPlans() {
+
         List<Plan> planList = planRepository.findAll();
 
-
-        List<PlanAllResponseDto> result = planList.stream().
-                map(plan -> new PlanAllResponseDto(plan))
+        List<PlanAllResponseDto> result = planList.stream()
+                .map(plan -> new PlanAllResponseDto(plan, replyRepository.findAllByPlan(plan)))
                 .collect(Collectors.toList());
+
         return result;
     }
 
@@ -45,8 +46,8 @@ public class PlanService {
         LocalDateTime end = LocalDateTime.now().withHour(23).withMinute(59).withSecond(59).withNano(999999999);
         List<Plan> planList = planRepository.findAllByCreatedAtBetween(start, end);
 
-        List<PlanAllResponseDto> result = planList.stream().
-                map(plan -> new PlanAllResponseDto(plan))
+        List<PlanAllResponseDto> result = planList.stream()
+                .map(plan -> new PlanAllResponseDto(plan, replyRepository.findAllByPlan(plan)))
                 .collect(Collectors.toList());
         return result;
     }
@@ -60,8 +61,8 @@ public class PlanService {
         LocalDateTime today = LocalDateTime.now().withHour(0).withMinute(0).withSecond(0).withNano(0);
         List<Plan> planList = planRepository.findAllByCreatedAtBefore(today);
 
-        List<PlanAllResponseDto> result = planList.stream().
-                map(plan -> new PlanAllResponseDto(plan))
+        List<PlanAllResponseDto> result = planList.stream()
+                .map(plan -> new PlanAllResponseDto(plan, replyRepository.findAllByPlan(plan)))
                 .collect(Collectors.toList());
         return result;
     }
@@ -81,7 +82,15 @@ public class PlanService {
     public PlanDetailResponseDto getPlan(Long id) {
         Plan plan = planRepository.findById(id).orElseThrow(
                 () -> new ApiRequestException("해당 Plan 글이 없습니다. id = " + id));
-        PlanDetailResponseDto responseDto = new PlanDetailResponseDto(plan);
+
+        List<Reply> replyList = replyRepository.findAllByPlan(plan);
+
+        List<ReplyResponseDto> replyResponseDtoList = replyList.stream()
+                .map(reply -> new ReplyResponseDto(reply))
+                .collect(Collectors.toList());
+
+
+        PlanDetailResponseDto responseDto = new PlanDetailResponseDto(plan, replyResponseDtoList);
         return responseDto;
     }
 
